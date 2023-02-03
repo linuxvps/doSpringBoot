89f463a5b42a653557f9cbd1a4431347910c4b7c
nima <linux.vps.vpn@gmail.com>
2023-02-03 15:16:52 +0330

    first commit spring boot

---
+++ src/main/java/com/dospringboot/dospringboot/DoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.doSpringBoot.doSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dospringboot/dospringboot/DoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doSpringBoot.doSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
