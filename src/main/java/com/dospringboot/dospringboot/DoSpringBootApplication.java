f5386e72ce60eb87463a5f7ca1de1bbf3758a142
nima <linux.vps.vpn@gmail.com>
2023-02-03 18:02:40 +0330

    make package name to small case

--- src/main/java/com/dospringboot/dospringboot/DoSpringBootApplication.java
+++ src/main/java/com/dospringboot/dospringboot/DoSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.doSpringBoot.doSpringBoot;
+package com.dospringboot.dospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
