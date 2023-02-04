06f07e3a30e4ff5c00fc36bc78614a8fc6d09545
nima <linux.vps.vpn@gmail.com>
2023-02-04 12:47:12 +0330

    delete useless directory

--- src/main/java/com/dospringboot/DoSpringBootApplication.java
+++ src/main/java/com/dospringboot/DoSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.dospringboot.dospringboot;
+package com.dospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
